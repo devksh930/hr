6eb7067705a9fe0a32bfc98fb020b32c19dc5e70
devksh930 <ksh930@icloud.com>
2024-11-23 04:23:03 +0900

    test : EmployeeQueryDao findEmployeeById 직원상세조회 test추가

--- src/test/java/me/devksh930/hr/infrastructure/query/EmployeeQueryDaoIntegrationTest.java
+++ src/test/java/me/devksh930/hr/infrastructure/query/EmployeeQueryDaoIntegrationTest.java
@@ -3,12 +3,14 @@ package me.devksh930.hr.infrastructure.query;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 
+import me.devksh930.hr.domain.model.EmployeeDetailQuery;
 import me.devksh930.hr.domain.model.EmployeeQuery;
 import me.devksh930.hr.test.config.DataBaseIntegrationTest;
 
@@ -19,7 +21,7 @@ class EmployeeQueryDaoIntegrationTest extends DataBaseIntegrationTest {
 
 	@Test
 	@DisplayName("EmployeeQuery 페이지 요청 시 결과가 반환")
-	void findEmployeePaged_shouldReturnPageResults() {
+	void findEmployeePaged() {
 		PageRequest pageRequest = PageRequest.of(
 			0,
 			10
@@ -43,9 +45,36 @@ class EmployeeQueryDaoIntegrationTest extends DataBaseIntegrationTest {
 
 	@Test
 	@DisplayName("전체 직원 수를 반환해야 한다.")
-	void testCountEmployee() {
+	void countEmployee() {
 		long count = employeeQueryDao.countEmployee();
 
 		assertThat(count).isGreaterThan(0);
+	}
+
+	@Test
+	@DisplayName("직원 ID로 직원 상세 정보를 반환해야 한다")
+	void findEmployeeById() {
+		Integer employeeId = 206;
+
+		Optional<EmployeeDetailQuery> employeeDetail = employeeQueryDao.findEmployeeById(employeeId);
+
+		assertThat(employeeDetail).isPresent();
+		EmployeeDetailQuery detail = employeeDetail.get();
+
+		assertThat(detail.id()).isEqualTo(employeeId);
+		assertThat(detail.firstName()).isNotBlank();
+		assertThat(detail.lastName()).isNotBlank();
+		assertThat(detail.jobTitle()).isNotBlank();
+		assertThat(detail.departmentName()).isNotBlank();
+	}
+
+	@Test
+	@DisplayName("존재하지 않는 직원의 정보를 조회 할경우")
+	void findEmployeeById_notFound() {
+		Integer employeeId = 0;
+
+		Optional<EmployeeDetailQuery> employeeDetail = employeeQueryDao.findEmployeeById(employeeId);
+
+		assertThat(employeeDetail).isEmpty();
 	}
 }
